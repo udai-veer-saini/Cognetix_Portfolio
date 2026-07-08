598432f3f78d93ea666cdc57296d720080b9a002
Udai Veer Saini <udaiveersaini12@gmail.com>
2026-07-08 21:47:27 +0530

    Final portfolio polish

---
+++ script.js
@@ -0,0 +1,45 @@
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{
+    threshold:0.2
+});
+
+const hiddenElements=document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((el)=>{
+    observer.observe(el);
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
